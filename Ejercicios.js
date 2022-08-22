0eb6e468dd6545143103d46b4106d891c4492da7
anitamartinez0 <anapm1712@gmail.com>
2022-08-21 21:14:42 -0300

    listo

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,14 +1,25 @@
-function Queue() {
+
     // Implementa la clase Queue
     // enqueue:   Agrega un valor a la queue.   Respeta el orden existente.
-    // dequeue:   Remueve un valor de la queue.   Obedece a FIFO y respeta el underflow (cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
+    // dequeue:   Remueve un valor de la queue.   Obedece a FIFO y respeta el underflow 
+    //(cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
     // size:   Devuelve el número de elementos que contiene la queue.
-    this.array = [];
-}
+    function Queue() {
+
+        this.array = [];
+
+        Queue.prototype.enqueue = function(elemento){
+            return this.array.push(elemento)
+        }
+        Queue.prototype.dequeue = function(elemento){
+            return this.array.shift(elemento)
+        }
+        Queue.prototype.size = function(elemento){
+            return this.array.length(elemento)
+        }
+    }
 
 
-
-function cardGame() {
     // Implementar la función cardGame: a partir de dos Queue que va a recibir como paráemtros tiene
     // que determinar quién será el ganador del juego de cartas. Las reglas de dicho juego son las siguientes:
     //    - Cada jugador tendrá un mazo con cartas numeradas del 1 al 12
@@ -28,11 +39,45 @@ function cardGame() {
     //     A --> 4  vs  6 <-- B [6 > 4 entones gana la mano B y pone ambas cartas en su mazo, colocando primero la suya]
     //    - mazoUserA = [2,10,11]
     //    - mazoUserB = [6,9,10,3,6,4]
-}
 
-function sets() {
+    function cardGame(mazoUserA,mazoUserB) {
+
+        for(var i=0;i<mazoUserA.length;i++){
+            for(var j=0;j<mazoUserB.length;j++){
+                if(mazoUserA[i]>mazoUserB[j]){
+                    mazoUserA.push(mazoUserA[i],mazoUserB[j])
+                }else{
+                    mazoUserB.push(mazoUserB[j], mazoUserA[i])
+                }
+            }
+        }
+
+        if(mazoUserA.length===mazoUserB,length){
+            return "Game tie!"
+        }
+        if(mazoUserA.length===0){
+            return "B wins"
+        }
+        if(mazoUserB.length===0){
+            return "A wins"
+        }
+
+    }
+
+
     // Implementa el metodo sets    
     // Usar el objeto set para obtener la cantidad de elementos iguales en dos arreglos
     // Muestra la cantidad
     // Aqui tu codigo
-}
+
+    function sets(arreglo1,arreglo2) {
+
+        var conjunto1= new Set(arreglo1)
+        var conjunto2= new Set(arreglo2)
+
+        var interseccion = new Set([...conjunto1]).filter(e=>conjunto2.has(e))
+        return interseccion.size()
+    }
+
+    console.log(sets([1,2,3,4],[0,3,2,6]))
+
